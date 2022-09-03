0fd5db04e04a8a01153ceea5db89c4b57191431c
jung-ho-1231 <parkjung-1231@naver.com>
2022-09-04 03:59:05 +0900

    in, !in 예제 추가

--- src/main/kotlin/lec05/Main.kt
+++ src/main/kotlin/lec05/Main.kt
@@ -31,3 +31,15 @@ fun getGrade(score: Int) :String {
         return "D"
     }
 }
+
+fun validateScore (score :Int) {
+    if (0 in 0..100) {
+        println("0부터 100 안에 포함됩니다.")
+    }
+}
+
+fun notValidateScore(score: Int) {
+    if (score !in 0..100) {
+        println("0부터 100까지 포함되지 않는 숫자입니다.")
+    }
+}

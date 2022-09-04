c5e1ddb7059f1fccbc45e81d714419ab71a3485e
jung-ho-1231 <parkjung-1231@naver.com>
2022-09-04 11:00:19 +0900

    if else 예제 추가

---
+++ src/main/kotlin/lec08/Main.kt
@@ -0,0 +1,7 @@
+package lec08
+
+class Main {
+
+}
+
+fun max (a :Int , b :Int) = if (a > b) a else b
---
+++ src/main/kotlin/lec08/MainJava.java
@@ -0,0 +1,12 @@
+package lec08;
+
+public class MainJava {
+
+    public int max(int a, int b) {
+        if (a > b) {
+            return a;
+        }
+        return b;
+    }
+}
+

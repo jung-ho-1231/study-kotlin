0050e795b5f77e84d143017f4d909d0a42db3eac
jung-ho-1231 <parkjung-1231@naver.com>
2022-09-04 10:13:50 +0900

    반복문

---
+++ src/main/kotlin/let06/Main.kt
@@ -0,0 +1,14 @@
+package let06
+
+fun main() {
+    // 컬렉션 반복문
+    val numbers = listOf(1L, 2L, 3L)
+    for (number in numbers) {
+        println(number)
+    }
+
+    for (i in 1..3) {
+        println(i)
+    }
+
+}
---
+++ src/main/kotlin/let06/MainJava.java
@@ -0,0 +1,18 @@
+package let06;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainJava {
+    public static void main(String[] args) {
+
+        List<Long> number = Arrays.asList(1L, 2L, 3L);
+        for (Long aLong : number) {
+            System.out.println("aLong = " + aLong);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(i);
+        }
+    }
+}

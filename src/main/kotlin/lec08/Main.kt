b2f8bb523c5adf4c01f80c4d8e4ca0a8e7696674
jung-ho-1231 <parkjung-1231@naver.com>
2022-09-04 11:09:24 +0900

    named parameter 예제

--- src/main/kotlin/lec08/Main.kt
+++ src/main/kotlin/lec08/Main.kt
@@ -2,6 +2,10 @@ package lec08
 
 fun main() {
     repeat("hello world")
+
+    // 중간에 있는 num 은 그대로 사용하고 싶은 경우에는?
+    // named argument 라는 함수를 호출하는 쪽에서 파라미터 이름을 명시해준다
+    repeat("hell world" , userNewLine = false)
 }
 fun max (a :Int , b :Int) = if (a > b) a else b
 

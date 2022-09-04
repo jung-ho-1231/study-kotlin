ec510041983e6eade929475b50e893582721b0e3
jung-ho-1231 <parkjung-1231@naver.com>
2022-09-04 10:29:03 +0900

    try catch 예제

---
+++ src/main/kotlin/lec07/Main.kt
@@ -0,0 +1,13 @@
+package lec07
+
+import java.lang.IllegalArgumentException
+import java.lang.NumberFormatException
+import java.util.StringJoiner
+
+fun parseIntoOrThrow(str: String): Int {
+    try {
+        return str.toInt()
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("주어진 ${str}은 숫자가 아닙니다.")
+    }
+}
---
+++ src/main/kotlin/lec07/MainJava.java
@@ -0,0 +1,18 @@
+package lec07;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MainJava {
+    public static void main(String[] args) {
+
+    }
+
+    private  int parseIntOrThrow(@NotNull String str){
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(String.format("주어진 %s는 숫자가 아닙니다", str));
+        }
+    }
+}
+

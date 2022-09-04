2de3f06c6efdc4e50d79911e2cbba7ba489d8c48
jung-ho-1231 <parkjung-1231@naver.com>
2022-09-04 11:07:33 +0900

    default parameter 예제

--- src/main/kotlin/lec08/Main.kt
+++ src/main/kotlin/lec08/Main.kt
@@ -1,7 +1,17 @@
 package lec08
 
-class Main {
-
+fun main() {
+    repeat("hello world")
 }
-
 fun max (a :Int , b :Int) = if (a > b) a else b
+
+// default parameter
+fun repeat(str: String, num: Int = 3, userNewLine: Boolean = true) {
+    for (i in 1..num) {
+        if (userNewLine) {
+            println(str)
+        } else {
+            println(str)
+        }
+    }
+}
--- src/main/kotlin/lec08/MainJava.java
+++ src/main/kotlin/lec08/MainJava.java
@@ -8,5 +8,26 @@ public class MainJava {
         }
         return b;
     }
+
+
+    public void repeat(String str, int num, boolean userNewLine) {
+        for (int i = 0; i < num; i++) {
+            if (userNewLine) {
+                System.out.println(str);
+            } else {
+                System.out.println(str);
+            }
+        }
+    }
+
+    public void repeat(String str, int num) {
+        // repeat userNewLine 이 기본적으로 true 가 많이 사용된다면 자바에서는 오버로딩을 통해서 아래와 같이 구현한다.
+        repeat(str, num, true);
+    }
+
+    public void repeat(String str) {
+        // 마찬가지로 num 이 기본적으로 3을 많이 사용한다면 아래와 같이 변경할 수 있다.
+        repeat(str, 3, true);
+    }
 }
 

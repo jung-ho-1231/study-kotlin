e3a4e241e297e6c06e9961015f7d90009c0c87bd
jung-ho-1231 <parkjung-1231@naver.com>
2022-09-06 21:14:23 +0900

    접근 제어 예제

---
+++ src/main/kotlin/lec11/Main.kt
@@ -0,0 +1,20 @@
+package lec11
+
+val a = 3
+
+fun add(a: Int, b: Int): Int {
+    return a + b
+}
+
+class Cat private constructor()
+
+
+class Car(
+    internal val name: String,
+    private val owner: String,
+    _price: Int
+) {
+
+    var price = _price
+        private set
+}
